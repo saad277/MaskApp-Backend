9c3b8330e18f0697aab86a6cbd6e468c55d3401e
saad277 <saadsheikh27@gmail.com>
2021-08-08 14:03:14 +0500

    user dtos

---
+++ src/user/dtos/user-signup.dto.ts
@@ -0,0 +1,39 @@
+import {
+  IsString,
+  MinLength,
+  MaxLength,
+  //Matches,
+  IsNumber,
+  IsEmail,
+  IsEnum,
+  IsObject,
+  IsBoolean,
+} from 'class-validator';
+import { UserRoles } from '../../user/user.roles.enums';
+
+export class UserSignUpDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  UserName: string;
+
+  @IsEmail()
+  Email: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  //   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+  //     message: 'Password Too Weak',
+  //   })
+  Password: string;
+
+  @IsEnum(UserRoles)
+  @IsNumber()
+  Type: number;
+
+  @IsString()
+  @MinLength(10)
+  @MaxLength(20)
+  ProfileImg: string;
+}
