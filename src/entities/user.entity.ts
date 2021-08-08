6d8a8ac3540ae6c0a019186751e9b0bd0f9c9bb9
saad277 <saadsheikh27@gmail.com>
2021-08-08 13:45:43 +0500

    swagger

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,49 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+import { UserRoles } from '../user/user.roles.enums';
+
+@Entity()
+@Unique(['UserName', 'Email'])
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('increment')
+  Id: number;
+
+  @Column('varchar', {
+    nullable: false,
+    name: 'name',
+  })
+  UserName: string;
+
+  @Column('varchar', {
+    nullable: false,
+  })
+  Email: string;
+
+  @Column('varchar', {
+    nullable: false,
+  })
+  Password: string;
+
+  @Column('varchar', {
+    nullable: false,
+  })
+  Salt: string;
+
+  @Column('int', {
+    nullable: false,
+  })
+  Type: UserRoles;
+
+  async validatePassword(password: string): Promise<boolean> {
+    const hash = await bcrypt.hash(password, this.Salt);
+
+    return hash === this.Password;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Mask App')
+    .setDescription('Service App Api Description')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth', // This name here is important for matching up with @ApiBearerAuth() in your controller!
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
+
+
+//nest update -f -t latest
